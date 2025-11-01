d58f5128d42dedfefdef3e8b73fa170c22ebd603
nachiketh <support@manifoldailearning.in>
2025-11-01 22:14:37 +0530

    langgraph

--- 3-langgraph/2.hello-langgraph.py
+++ 3-langgraph/2.hello-langgraph.py
@@ -13,25 +13,28 @@ llm_gemini = ChatGoogleGenerativeAI(model="gemini-2.5-flash",
                                 max_output_tokens=2048)
 
 def llm_node(state: dict) -> dict:
-    user_input = state.get("input", "How are you?")
+    user_input = state.get("prompt")
+    initial_response = state["response"]
+    print("Initial Response:", initial_response)
     # user_input = state["input"]
     response = llm_gemini.invoke(user_input)
-    return {"output": response.content}
+    return {"response": response.content}
 
 class GraphState(TypedDict):
-    input: str
-    output: str
-
+    prompt: str
+    response: str
+    
 graph = StateGraph(GraphState)
 graph.add_node("llm", llm_node)
 graph.add_edge(START, "llm")
 graph.add_edge("llm", END)
 
-input_state = {"input": "Explain Langraph in one Sentence."}
+#input_state = {"prompt": "Explain Langraph in one Sentence."}
+input_state = {"prompt": "Explain Langraph in one Sentence.","response":"placeholder"}
 app = graph.compile()
 
 
 result = app.invoke(input_state)
-app.get_graph().draw_mermaid_png()
-print("Final Output:", result["output"])
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
+print("Final Output:", result["response"])
 
